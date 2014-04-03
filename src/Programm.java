99310ffa6198155077f3dd0cb6a3ad3f626db439
nik-gubin <nik-gubin@yandex.ru>
2014-04-03 17:37:35 +0400

    com

---
+++ src/Programm.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Николай
+ * Date: 03.04.14
+ * Time: 17:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Programm {
+}
